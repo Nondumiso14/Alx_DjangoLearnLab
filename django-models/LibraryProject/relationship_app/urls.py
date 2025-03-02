884dd9392d9b88d8cbe1cc084c60cbd95a2032fe
Nondumiso14 <Nondumisoshange3@gmail.com>
2025-03-03 00:16:43 +0200

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,3 +24,10 @@ urlpatterns = [
     
     path('register/', views.register_view, name='register.html')
 ]
+
+from django.urls import path
+from . import views
+urlpatterns = [path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -46,6 +46,7 @@ from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
 
 def is_admin(user):
     return user.userprofile.role == 'Admin'
@@ -58,15 +59,18 @@ def is_member(user):
 
 
 @login_required
+@user_passes_test(is_admin)
 def admin_view(request):
         return render(request, 'admin_view.html')
     
 @login_required
+@user_passes_test(is_librarian)
 def librarian_view(request):
         return render(request, 'librarian_view.html')
     
 
 @login_required
+@user_passes_test(is_member)
 def member_view(request):
         return render(request, 'member_view.html')
     
